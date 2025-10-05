1e41a7f1d48ba7877a62bd50aad15fe27f2b78ee
Maryam Data ScienceWali <maryambukhari698@gmail.com>
2025-10-05 17:34:19 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+// db.php
+$host = "localhost";
+$username = "uczrllawgyzfy";
+$password = "tmq3v2ylpxpl";
+$dbname = "dbxfngey24pxcd";
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
